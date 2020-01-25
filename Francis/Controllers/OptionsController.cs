31600196409c4dda9036b987bb8c67a80a271f66
Namaneo <mini.jul.jl@gmail.com>
2020-01-25 14:24:32 +0100

    Test all Telegram configurations

--- Francis/Controllers/OptionsController.cs
+++ Francis/Controllers/OptionsController.cs
@@ -7,6 +7,8 @@ using Francis.Telegram.Client;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Francis.Controllers
@@ -52,6 +54,8 @@ namespace Francis.Controllers
         {
             _updater.Save(options);
             _bot.Initialize();
+            await _bot.Client.SendTextMessageAsync(options.AdminChat, "Hello Francis' administrator, if you received this message, this means your configuration is valid.");
+            if (!Directory.Exists(options.MediaLocation)) throw new ArgumentException("Invalid media location");
             return await _bot.Client.GetMeAsync();
         }
 
